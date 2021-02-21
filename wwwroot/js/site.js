a04075b3bd33fffd21acb9d43cfccfa8781140d8
Michael Stevenson <michael.stevenson@finance-ni.gov.uk>
2021-02-21 14:21:20 +0000

    Bumped jquery versions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,14 @@
-﻿// code for keeping footer at a nice height.
+﻿$(document).ready(function () {
+    // Show error_summary if there any validation errors, this is so the summary shows for both front and back end validation.
+    if ($(".error-items").length) {
+        $(".govuk-error-summary").show();
+    }
+});
+
 function adjustFooterHeight() {
-    var docHeight = $(window).height();
-    var footerHeight = $('.nidirect_footer').height();
-    var footerTop = $('.nidirect_footer').position().top + footerHeight;
+    let docHeight = $(window).height();
+    let footerHeight = $('.nidirect_footer').height();
+    let footerTop = $('.nidirect_footer').position().top + footerHeight;
 
     if (footerTop < docHeight) {
         $('.nidirect_footer').css('margin-top', -20 + (docHeight - footerTop) + 'px');
